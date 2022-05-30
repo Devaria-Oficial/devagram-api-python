cb1c52c3a71d50bcc8368912e39d9a7a126a39e3
Filipe Campos <filipefcl@hotmail.com>
2022-05-30 10:47:28 -0300

    Correção

--- Server.py
+++ Server.py
@@ -5,4 +5,10 @@ from decouple import config
 if __name__ == "__main__":
     PORT = config("MONGODB_URL")
 
+    if not PORT:
+        PORT = 5000
+    
+
+    PORT = int(PORT)    
+
     uvicorn.run('Main:app', host="0.0.0.0", port=PORT, reload=True)
